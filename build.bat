e028ed50b20136e2675ffaef84559e4b6e7a6a04
Shirai_Kuroko <73374735+IShiraiKurokoI@users.noreply.github.com>
2024-07-11 14:58:24 +0800

    Optimize 0.9.0 IO module patch
    Only block those part using turbojpeg

--- build.bat
+++ build.bat
@@ -1,3 +1,4 @@
+@echo off
 mkdir build & cd build
-cmake -G Ninja -DCMAKE_INSTALL_PREFIX=../install .. && cmake --build .
+cmake -G Ninja -DANDROID_NDK="C:\Users\Shirai_Kuroko\AppData\Local\Android\Sdk\ndk\20.1.5948944" -DCMAKE_INSTALL_PREFIX=../install .. && cmake --build .
 cd ..
